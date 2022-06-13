59eb110008084b003b191052ebc051874dacad44
lllrdgz <lllrdgz.cs@gmail.com>
2022-06-13 13:32:51 -0400

    Test fixed

--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SchoolManagementSystem.Domain.Entities;
+
+namespace Test;
+
+public class Program
+{
+    public static void Main()
+    {
+        Student student = new Student("123456784012", "Pablo", "Curbelo Paez", 56784392, 
+            "Pocitos No.23 e/ Czda de Vento y ALmendares",new DateTime(2020,2,1),
+            new Tuitor{Name="Elena", PhoneNumber=54637721},3, SchoolManagementSystem.Domain.Enums.Education.Posgrado);
+    }
+}
