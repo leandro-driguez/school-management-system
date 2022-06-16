c3777e6b4a2cb4846b4af1744710d33b5f11b35b
lllrdgz <lllrdgz.cs@gmail.com>
2022-06-15 21:09:38 -0400

    SchoolManagementSystem.Bootstrapper project added

---
+++ src/SchoolManagementSystem.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SchoolManagementSystem.Bootstrapper;
+
+public static class Bootstrapper
+{
+    public static void Boot(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+    }
+}
