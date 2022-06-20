b2ad7b846695ac5dd81775dcdf3fd901ae96e83c
Lázaro D <66334895+LazaroDGM@users.noreply.github.com>
2022-06-20 19:46:15 -0400

    servicio de Expense extendido con RecordExpense

--- src/SchoolManagementSystem.Application/Services_Implementations/ExpenseService.cs
+++ src/SchoolManagementSystem.Application/Services_Implementations/ExpenseService.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.EntityFrameworkCore;
 using SchoolManagementSystem.Application.Repositories_Interfaces;
 using SchoolManagementSystem.Domain.Entities;
 using SchoolManagementSystem.Domain.Services;
@@ -10,5 +11,10 @@ public class ExpenseService : BaseService<Expense>, IExpenseService
     public ExpenseService(IExpenseRepository repository) : base(repository)
     {
 
+    }
+
+    public override IQueryable<Expense> Query()
+    {
+        return base.Query().Include(e => e.ExpenseRecords).AsNoTracking();
     }
 }
