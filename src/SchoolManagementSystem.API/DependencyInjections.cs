f5ad764d5343bb6452e055c88f191b3f166e8c15
Marcos Tirador <marcosmath4@gmail.com>
2022-06-21 23:03:39 -0400

    pinga el vscode este

---
+++ src/SchoolManagementSystem.API/DependencyInjections.cs
@@ -0,0 +1,55 @@
+// using Gateways.Data.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SchoolManagementSystem.API;
+
+
+public static class DependencyInjections
+{
+    public static void SetUpMyServicesDependencyInjections(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var abstracts = AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .Where(x => x.Namespace == "SchoolManagementSystem.Domain.Services")
+            .Where(x => x.IsInterface)
+            .Where(x => !x.ContainsGenericParameters)
+            .ToList();
+        foreach (var item in abstracts)
+            BootType(services, item);
+    }
+
+    private static void BootType(IServiceCollection services, Type type)
+    {
+        if (services.Any(x => x.ServiceType == type))
+            return;
+        var impls = AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .Where(x => x.IsAssignableTo(type))
+            .Where(x => x.IsClass)
+            .Where(x => !x.IsAbstract)
+            .ToList();
+        if (impls.Count > 1)
+            throw new Exception($"{type.Name} has more than one implementation");
+        if (impls.Count == 0)
+            return;
+        var impl = impls.First();
+        services.AddScoped(type, impl);
+        var parameterTypes = impl
+            .GetConstructors()
+            .SelectMany(t => t.GetParameters())
+            .Select(p => p.ParameterType)
+            .ToList();
+        foreach (var parameterType in parameterTypes)
+            BootType(services, parameterType);
+    }
+}
+
+
+}
