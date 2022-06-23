b39a45fdfad438f9e4ad518b0d0cf2e8ac3d72a1
Marcos Tirador <marcosmath4@gmail.com>
2022-06-23 03:07:15 -0400

    Removing trash comments

--- src/SchoolManagementSystem.API/Controllers/RelationCruds/WorkerPositionRelationController.cs
+++ src/SchoolManagementSystem.API/Controllers/RelationCruds/WorkerPositionRelationController.cs
@@ -27,21 +27,6 @@ public class WorkerPositionRelationController : Controller
     public IActionResult GetAll()
     {
         var _query = _service.Query().Include(c => c.Worker).Include(c => c.Position);
-        // List<StudentCourseGroupRelationDto> output = new List<StudentCourseGroupRelationDto>();   
-        // foreach (var item in _query)
-        // {
-        //     StudentCourseGroupRelationDto dto = new StudentCourseGroupRelationDto(){
-        //        CourseGroupCourseId = item.CourseGroupCourseId,
-        //        CourseGroupCourseName = item.CourseGroup.Course.Name,
-        //        CourseGroupName = item.CourseGroup.CourseGroupName,
-        //        StudentName = item.Student.Name,
-        //        StudentId = item.StudentId,
-        //        CourseGroupId = item.CourseGroupId,
-        //        EndDate = item.EndDate,
-        //        StartDate = item.StartDate               
-        //     };
-        //     output.Add(dto);
-        // }
         return Ok(_query.Select(mapper.Map<WorkerPositionRelation,WorkerPositionRelationDto>));
     }
 
@@ -51,14 +36,6 @@ public class WorkerPositionRelationController : Controller
         if(!_service.ValidateIds(dto.WorkerId, dto.PositionId))
             return NotFound();
         _service.Add(mapper.Map<WorkerPositionRelation>(dto));
-        // _service.Add(new StudentCourseGroupRelation
-        // {
-        //     CourseGroupId = dto.CourseGroupId,
-        //     StudentId = dto.StudentId,
-        //     CourseGroupCourseId = dto.CourseGroupCourseId,
-        //     StartDate = dto.StartDate,
-        //     EndDate = dto.EndDate
-        // });
         _service.CommitAsync();
         return Ok();
     }
@@ -69,14 +46,6 @@ public class WorkerPositionRelationController : Controller
         if(!_service.ValidateIds(dto.WorkerId, dto.PositionId))
             return NotFound();
         var relation = mapper.Map<WorkerPositionRelation>(dto);
-        // var relation = new StudentCourseGroupRelation
-        // {
-        //     CourseGroupId = dto.CourseGroupId,
-        //     StudentId = dto.StudentId,
-        //     CourseGroupCourseId = dto.CourseGroupCourseId,
-        //     StartDate = dto.StartDate,
-        //     EndDate = dto.EndDate
-        // };
         _service.Update(relation);
         _service.CommitAsync();
         return Ok();
