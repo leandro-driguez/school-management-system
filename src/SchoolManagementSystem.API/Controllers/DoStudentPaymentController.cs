80db029447dadc2abde01d1f39e80d17199827d7
Lázaro D <66334895+LazaroDGM@users.noreply.github.com>
2022-06-24 00:31:51 -0400

    annadido el post sin implementar completo
    solamente verifica si estas en los records, cual es la ultima fecha que tienes pagada

--- src/SchoolManagementSystem.API/Controllers/DoStudentPaymentController.cs
+++ src/SchoolManagementSystem.API/Controllers/DoStudentPaymentController.cs
@@ -27,29 +27,56 @@ public class DoStudentPaymentController : Controller
         this.mapper = mapper;
     }
 
-    [HttpGet]
-    public IActionResult GetAll()
+    [HttpGet("{id}")]
+    public IActionResult GetAll(string id)
     {
-        // cursos y estudiantes de las relaciones
-        //var q1 = from relation in _serviceRelation.Query()
-        //         select new CourseStudent
-        //         {
-        //             CourseId = relation.CourseGroup.CourseId,
-        //             StudentId = relation.StudentId
-        //         };
-        //// cursos y estudiantes pagados 
+        // cursos-grupo que ya ha ido pagando el estudiante
+        var q1 = from record in _serviceRecord.Query()
+                 where record.StudentId == id
+                 select record.CourseGroupId;
+
+        
         //var q2 = from record in _serviceRecord.Query()
-        //         select new CourseStudent
-        //         {
-        //             CourseId = record.CourseGroup.CourseId,
-        //             StudentId = record.StudentId
-        //         };
+        //         group record by record.CourseGroupId into g
+        //         select g.OrderByDescending(e => e.CourseGroupId).FirstOrDefault() into r
+        //         select r;
 
-        //var q3 = from relation in q1.AsEnumerable()
-        //         where !q2.AsEnumerable().Contains(relation)
-        //         select relation;
+        // cursos-grupo en los que no ha pagado aún
+        // solo tiene los que nunca ha pagado ni una vez
+        var q3 = from relation in _serviceRelation.Query()
+                 where !q1.Contains(relation.CourseGroupId)
+                 select relation;
 
-        //return Ok(q3.ToList());
+        return Ok(q3.ToList());
         //return Ok(new IList[] { q3.ToList(), q4.ToList(), q5.ToList() });
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromForm] StudentIdGroupIdDto dto)
+    {
+        var q1 = from record in _serviceRecord.Query()
+                 group record by record.CourseGroupId into g
+                 select new
+                 {
+                     StudentId = g.Select(r => r.StudentId).FirstOrDefault(),
+                     CourseGroupId = g.Select(r => r.CourseGroupId).FirstOrDefault(),
+                     CourseGroupCourseId = g.Select(r => r.CourseGroupCourseId).FirstOrDefault(),
+                     Date = g.Select(r => r.Date).FirstOrDefault(),
+                     DatePaid = g.Max(r=>r.DatePaid)
+                 };
+        var q2 = from record in q1
+                 where record.StudentId == dto.StudentId &&
+                        record.CourseGroupId == dto.GroupId
+                 select record;
+
+        var ultimo_mes_pagado = q2.FirstOrDefault();
+        if (ultimo_mes_pagado == null)
+        {
+            // antes habria que comprobar si está en la tabla de relaciones
+            // para entonces incluirlo en los records
+            return NotFound(dto);
+        }
+        // Faltaría meterlo como tal ahora
+        throw new NotImplementedException();
     }
 }
