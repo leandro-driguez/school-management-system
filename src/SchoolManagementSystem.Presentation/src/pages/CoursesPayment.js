26b85cee954787dad7b3bd624d81cfa83e8325be
LRiera24 <laura.riera2412@gmail.com>
2022-06-24 10:25:47 -0400

    Collapse Test

--- src/SchoolManagementSystem.Presentation/src/pages/CoursesPayment.js
+++ src/SchoolManagementSystem.Presentation/src/pages/CoursesPayment.js
@@ -1,10 +1,27 @@
 import React from "react";
 import NavBar from "../components/NavBar/NavBar";
+import CollapsePanels from "../components/CollapsePanels/CollapsePanels";
+
+import {Collapse, Table} from "antd";
 
 const CoursesPayment = () => {
+    const components = () => {
+        return (
+            <div>
+        <Table title={"JNDFKJSN"}></Table>
+        <Table title={"JNDFKJSN"}></Table>
+        <Table title={"JNDFKJSN"}></Table>
+            </div>
+    );
+    };
+
     return (
         <div>
             <NavBar></NavBar>
+
+            <Collapse ghost>
+                <CollapsePanels titles={["a", "b", "c"]} components={components()}></CollapsePanels>
+            </Collapse>
         </div>
     );
 };
