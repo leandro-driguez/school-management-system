38f5c35ea47225bef8d3f317d2d929ce1b589cdd
lllrdgz <lllrdgz.cs@gmail.com>
2022-06-25 18:05:53 -0400

    fixup! added fixed salary to position per worker.

---
+++ src/SchoolManagementSystem.API/Controllers/RelationCruds/PositionSalaryController.cs
@@ -0,0 +1,43 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.API.Dtos;
+using SchoolManagementSystem.API.Mappers;
+using SchoolManagementSystem.Domain.Entities;
+using SchoolManagementSystem.Domain.Services;
+using SchoolManagementSystem.Domain.Relations;
+using Microsoft.EntityFrameworkCore; 
+using AutoMapper;
+
+namespace SchoolManagementSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PositionSalaryController : Controller
+{
+    public readonly IWorkerPositionRelationService _service;
+    public readonly IMapper mapper;
+    
+    public PositionSalaryController(IWorkerPositionRelationService service, IMapper mapper)
+    {
+        _service = service;
+        this.mapper = mapper;
+    }
+    [HttpGet("{id}")]
+    public IActionResult Get(string id)
+    {
+        var _query = _service.Query().Where(c=>c.WorkerId == id).Include(c => c.Worker).Include(c => c.Position);
+        var listofPositionSalary = new List<PositionSalaryRelDto>();
+        foreach (var row in _query)
+        {
+            PositionSalaryRelDto single = new PositionSalaryRelDto
+            {
+                Position = row.Position.Name,
+                FixedSalary = row.FixedSalary
+            };
+            listofPositionSalary.Add(single);
+        }
+        return Ok(listofPositionSalary);
+    }
+
+}
