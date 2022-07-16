e34161e23ac876c59130c7f0064fb9c75541b2ef
Marcos Tirador <marcosmath4@gmail.com>
2022-07-16 19:14:35 -0400

    TeacherId addded as key property of teacherCourseRelation

--- src/SchoolManagementSystem.API/Dtos/Relations/TeacherCourseRelationDto.cs
+++ src/SchoolManagementSystem.API/Dtos/Relations/TeacherCourseRelationDto.cs
@@ -1,13 +1,14 @@
 
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using SchoolManagementSystem.Domain.Entities;
-
 
 namespace SchoolManagementSystem.API.Dtos;
 
 public class TeacherCourseRelationDto
 {
     [Required]
+    [JsonPropertyName("key")]
     public string TeacherId { get; set; }
     // [Required]
     public string TeacherName { get; set; }
